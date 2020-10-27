b1ee6f90c328442e297a96d3da6f07d1d665cc10
Jeremy Bauer <jeremybbauer@gmail.com>
2020-10-27 15:56:45 -0400

    Corrected pagination

--- src/templates/home.js
+++ src/templates/home.js
@@ -44,33 +44,48 @@ export default class Home extends React.Component {
                 </article>
                 ))}
               </div>
+              <ul
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            listStyle: 'none',
+            padding: 0,
+          }}
+        >
+          {!isFirst && (
+            <Link to={prevPage} rel="prev">
+              ← Previous Page
+            </Link>
+          )}
+          {Array.from({ length: numPages }, (_, i) => (
+            <li
+              key={`pagination-number${i + 1}`}
+              style={{
+                margin: 0,
+              }}
+            >
+              <Link
+                to={`/${i === 0 ? '' : i + 1}`}
+                style={{
+                  padding: rhythm(1 / 4),
+                  textDecoration: 'none',
+                  color: i + 1 === currentPage ? '#ffffff' : '',
+                  background: i + 1 === currentPage ? '#007acc' : '',
+                }}
+              >
+                {i + 1}
+              </Link>
+            </li>
+          ))}
+          {!isLast && (
+            <Link to={nextPage} rel="next">
+              Next Page →
+            </Link>
+          )}
+        </ul>
             </Layout>
         );
     }
-}
-class BlogList extends React.component {
-  render() {
-    const { currentPage, numPages } = this.props.pageContext
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numPages
-    const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
-    const nextPage = (currentPage + 1).toString()
-    return (
-       /* your code to display a list of posts */
-      {!isFirst && (
-        <Link to={prevPage} rel="prev">
-          ← Previous Page
-        </Link>
-      )}
-      {!isLast && (
-        <Link to={nextPage} rel="next">
-          Next Page →
-        </Link>
-        {Array.from({ length: numPages }, (_, i) => (
-          <Link key={`pagination-number${i + 1}`} to={`/${i === 0 ? "" : i + 1}`}>
-            {i + 1}
-          </Link>
-      )}
-    )
-  }
 }
