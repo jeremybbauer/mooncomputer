7fc57a3e5f0d650a0be8426f61639803c7ad3d15
Jeremy Bauer <jeremybbauer@gmail.com>
2020-10-27 15:34:01 -0400

    Add pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+// Create blog post list pages
+const postsPerPage = 7;
+const numPages = Math.ceil(posts.length / postsPerPage);
+Array.from({ length: numPages }).forEach((_, i) => {
+  createPage({
+    path: i === 0 ? `/` : `/${i + 1}`,
+    component: path.resolve("./src/templates/home.js"),
+    context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+    },
+  });
+});
--- src/templates/home.js
+++ src/templates/home.js
@@ -48,3 +48,29 @@ export default class Home extends React.Component {
         );
     }
 }
+class BlogList extends React.component {
+  render() {
+    const { currentPage, numPages } = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
+    const nextPage = (currentPage + 1).toString()
+    return (
+       /* your code to display a list of posts */
+      {!isFirst && (
+        <Link to={prevPage} rel="prev">
+          ← Previous Page
+        </Link>
+      )}
+      {!isLast && (
+        <Link to={nextPage} rel="next">
+          Next Page →
+        </Link>
+        {Array.from({ length: numPages }, (_, i) => (
+          <Link key={`pagination-number${i + 1}`} to={`/${i === 0 ? "" : i + 1}`}>
+            {i + 1}
+          </Link>
+      )}
+    )
+  }
+}
